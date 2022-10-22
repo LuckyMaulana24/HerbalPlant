f64e7011c8882a449dd0fff085017f115adc63ed
LuckyMaulana24 <luckymaulana35@gmail.com>
2022-10-22 15:58:47 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Herbal Plant Apps',
+      title: 'Herbal Plant Apps 1',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Herbal Plant'),
