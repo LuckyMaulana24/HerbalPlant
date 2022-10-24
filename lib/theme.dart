190659339d976db21003ecc5da76a05ac0ecabbb
LuckyMaulana24 <luckymaulana35@gmail.com>
2022-10-24 15:00:14 +0700

    Menambahkan theme.dart untuk tema aplikasi

---
+++ lib/theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kGreenColor = const Color(0xff76BA99);
+Color kGreenDarkColor = const Color(0xff244C5B);
+Color kBlackColor = const Color(0x0ff00000);
+Color kWhiteColor = const Color(0xffffffff);
+
+TextStyle greenTextStyle = GoogleFonts.nunito(
+  color: kGreenColor,
+);
+TextStyle blackTextStyle = GoogleFonts.nunito(
+  color: kBlackColor,
+);
+TextStyle greenDarkTextStyle = GoogleFonts.nunito(
+  color: kGreenDarkColor,
+);
+TextStyle whiteTextStyle = GoogleFonts.nunito(
+  color: kWhiteColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight reguler = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semibold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
