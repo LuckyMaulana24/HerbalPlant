7d72550698a510b01a267af2a56a18a807213414
LuckyMaulana24 <luckymaulana35@gmail.com>
2022-10-27 01:02:23 +0700

    menyelesaikan slicing sign in

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -15,6 +15,20 @@ class SignInPage extends StatelessWidget {
             title(),
             emailInput(),
             passwordInput(),
+            forgotPassword(),
+            loginButton(),
+            Center(
+              child: Container(
+                margin: const EdgeInsets.only(top: 24),
+                child: Text(
+                  'Or, login with',
+                  style: greenTextStyle.copyWith(
+                      fontSize: 14, fontWeight: semibold),
+                ),
+              ),
+            ),
+            account(),
+            createAccount(),
           ],
         ),
       ),
@@ -104,6 +118,132 @@ class SignInPage extends StatelessWidget {
             ),
           ),
           const Icon(Icons.visibility_outlined)
+        ],
+      ),
+    );
+  }
+
+  Widget forgotPassword() {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            'forgot password?',
+            style: greenDarkTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: semibold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget loginButton() {
+    return Container(
+      margin: const EdgeInsets.only(top: 32),
+      height: 56,
+      width: double.infinity,
+      child: TextButton(
+        onPressed: () {},
+        style: TextButton.styleFrom(
+          backgroundColor: kGreenColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(14),
+          ),
+        ),
+        child: Text(
+          'Login',
+          style: whiteTextStyle.copyWith(fontSize: 18, fontWeight: semibold),
+        ),
+      ),
+    );
+  }
+
+  Widget account() {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            height: 40,
+            width: 80,
+            decoration: BoxDecoration(
+              color: kWhiteColor,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: kGreenColor,
+                  spreadRadius: 1,
+                ),
+              ],
+            ),
+            child: const Image(
+              image: AssetImage('assets/google.png'),
+            ),
+          ),
+          Container(
+            height: 40,
+            width: 80,
+            decoration: BoxDecoration(
+              color: kWhiteColor,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: kGreenColor,
+                  spreadRadius: 1,
+                ),
+              ],
+            ),
+            child: const Image(
+              image: AssetImage('assets/fb.png'),
+            ),
+          ),
+          Container(
+            height: 40,
+            width: 80,
+            decoration: BoxDecoration(
+              color: kWhiteColor,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: kGreenColor,
+                  spreadRadius: 1,
+                ),
+              ],
+            ),
+            child: const Image(
+              image: AssetImage('assets/apple.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget createAccount() {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Don\'t have an account yet?',
+            style: greenTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: semibold,
+            ),
+          ),
+          TextButton(
+              onPressed: () {},
+              child: Text(
+                'Register',
+                style:
+                    greenDarkTextStyle.copyWith(fontSize: 16, fontWeight: bold),
+              ))
         ],
       ),
     );
