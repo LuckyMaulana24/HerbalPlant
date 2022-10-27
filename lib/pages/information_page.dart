bbfaef8b292b05951d6f54343c9a7d0f2fa94d11
LuckyMaulana24 <luckymaulana35@gmail.com>
2022-10-27 23:07:19 +0700

    menambahkan information page

--- lib/pages/information_page.dart
+++ lib/pages/information_page.dart
@@ -1,6 +1,5 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
+import 'package:herbal_plant/theme.dart';
 
 class InformationPage extends StatelessWidget {
   const InformationPage({Key? key}) : super(key: key);
@@ -12,31 +11,27 @@ class InformationPage extends StatelessWidget {
         children: [
           Expanded(
             flex: 1,
-            // ignore: avoid_unnecessary_containers
             child: Container(
-              child: Image.asset('asset/images/logo.png'),
+              child: Image.asset('assets/background.png'),
             ),
           ),
           Expanded(
             flex: 1,
             child: Container(
               decoration: BoxDecoration(
-                color: Color(0xff0C3B2E),
-                borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(10),
-                  topLeft: Radius.circular(10),
+                color: kGreenColor,
+                borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(30),
+                  topLeft: Radius.circular(30),
                 ),
               ),
               child: Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: Text(
-                  'Harvest Sun adalah aplikasi mengenai tanaman herbal atau tanaman obat. Dimana tanaman obat memiliki banyak manfaat yang yang bagus untuk kesehatan tubuh. Aplikasi ini menggunakan sumber buku yaitu "Petunjuk Praktis Penggunaan Tumbuhan Untuk Pengobatan sebagai solusi tepat menuju hidup sehat" yang di terbitkan oleh Lembaga Penelitian Pengembangan Dan Pemberdayaan Masyarakat[LP3M]',
-                  style: TextStyle(
-                    decoration: TextDecoration.none,
-                    fontFamily: 'Rowdies',
+                  'Herbal Plant adalah aplikasi mengenai tanaman herbal atau tanaman obat. Dimana tanaman obat memiliki banyak manfaat yang yang bagus untuk kesehatan tubuh. Aplikasi ini menggunakan sumber buku yaitu "Petunjuk Praktis Penggunaan Tumbuhan Untuk Pengobatan sebagai solusi tepat menuju hidup sehat" yang di terbitkan oleh Lembaga Penelitian Pengembangan Dan Pemberdayaan Masyarakat[LP3M]',
+                  style: whiteTextStyle.copyWith(
                     fontSize: 20,
-                    //fontWeight: FontWeight.bold,
-                    color: Color(0xffF8FAF8),
+                    fontWeight: bold,
                   ),
                   textAlign: TextAlign.center,
                 ),
